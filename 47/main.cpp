6bbcea1ee0555cdcda695bc4675e6025b673cf4d
Yuan <c14016057@gamil.com>
2019-05-16 21:43:14 +0800

    add problem 47

---
+++ 47/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+		std::sort(nums.begin(), nums.end());
+		vector<vector<int>> ret;
+		perm(nums, ret, 0);
+		return ret;
+	}
+private:
+	void perm(vector<int>& nums, vector<vector<int>>& ret, int idx) {
+		if (idx == nums.size()-1) {
+			ret.push_back(nums);
+			return;
+		}
+		for (int i = idx; i < nums.size(); i++) {
+			if(nums[idx] == nums[i]&& idx!=i)
+				continue;
+			int temp = nums[idx];
+			nums[idx] = nums[i];
+			nums[i] = temp;
+			perm(nums, ret, idx+1);
+		}
+		for (int i = idx; i < nums.size()-1; i++) {
+			int temp = nums[i];
+			nums[i] = nums[i+1];
+			nums[i+1] = temp;
+		}
+		return;
+	}
+};
+int main() {
+	int N;
+	cin >> N;
+	vector<int> vec;
+	while(N--) {
+		int temp;
+		cin >> temp;
+		vec.push_back(temp);
+	}
+	Solution sol;
+	vector<vector<int>> res = sol.permute(vec);
+	for (vector<int> a:res) {
+		for (int n : a)
+			cout << n << " ";
+		cout << endl;
+	}
+	return 0;
+}
